6fc3d78c167b0db9458fd8c8d73611bfdf4b8e0d
priya <priyanka.pawar165@gmail.com>
2024-02-26 23:04:02 -0500

    Added spring-boot-aop-validation

---
+++ spring-boot-aop-validation/src/main/java/com/SpringBootAopValidationApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopValidationApplication.class, args);
+	}
+
+}
